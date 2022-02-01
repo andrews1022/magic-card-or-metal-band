0a1ec1192cb3f49e124021272d09af3b7b125688
andrews1022 <andrewshearerdev@gmail.com>
2022-02-01 15:02:43 -0800

    removed svg files to use material icons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
+// material icons
+import CropPortraitIcon from '@material-ui/icons/CropPortrait';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
 // components
 import Game from '../Game';
 import Start from '../Start';
 
 const App = () => (
 	<div>
-		<h1>Hello from App.tsx!</h1>
 		<Game />
 		<Start />
+
+		<CropPortraitIcon />
+		<MusicNoteIcon />
+		<PlayArrowIcon />
 	</div>
 );
 
